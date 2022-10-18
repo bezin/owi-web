762f877ffcf4a78ab201ff3474beb42bb49cf567
Benedict Massolle <bm@presentprogressive.de>
2022-10-18 16:25:37 +0200

    Refactor Weekdays component

--- src/components/Weekdays.vue
+++ src/components/Weekdays.vue
@@ -14,25 +14,22 @@ const props = defineProps({
 })
 
 const dimensions = {
-  width: 0,
-  height: 0,
-  gap: 20,
+  width: 500,
+  height: 230,
   margins: {
     top: 16,
-    right: 16,
-    bottom: 32,
-    left: 16,
+    right: 24,
+    bottom: 16,
+    left: 24,
   },
   bar: {
-    width: 40,
-    height: 150,
+    width: 50,
+    height: 160,
   },
 }
 
-dimensions.width = (dimensions.bar.width * 7) + (dimensions.gap * 6) + dimensions.margins.left + dimensions.margins.right
-dimensions.height = dimensions.bar.height + dimensions.margins.top + dimensions.margins.bottom
-
-const graphData = {}
+// Dynamically calculate gap to spread out bars across SVG width
+dimensions.gap = ((dimensions.width - dimensions.margins.left - dimensions.margins.right) - (dimensions.bar.width * 7)) / 6
 
 const colors = ['#ffffff', '#eff6ff','#dbeafe','#bfdbfe','#93c5fd','#60a5fa','#3b82f6','#2563eb','#1d4ed8','#1e40af','#1e3a8a']
 
@@ -43,9 +40,12 @@ function colorScale(val) {
   return colors[(category - 1)]
 }
 
+const graphData = {}
+
 Object.keys(props.data).forEach(dateString => {
   const date = dayjs(dateString, 'YYYY-MM-DD')
-  const day = DAYS[date.day()]
+  const dayOfWeek = date.day()
+  let day = (dayOfWeek === 0) ? 7 : dayOfWeek
   
   if (!graphData[day]) {
     graphData[day] = 0
@@ -69,7 +69,7 @@ function getBarHeight(value, height) {
 <template>
   <svg :width="dimensions.width" :height="dimensions.height" :viewBox="`0 0 ${dimensions.width} ${dimensions.height}`">
     <g :transform="`translate(${dimensions.margins.left})`">
-      <g :transform="`translate(0 ${dimensions.height - 10})`" class="label">
+      <g :transform="`translate(0 ${dimensions.margins.top + dimensions.bar.height + 35})`" class="label">
         <text :x="dimensions.bar.width / 2" y="0" text-anchor="middle">Mo</text>
         <text :x="dimensions.bar.width + dimensions.bar.width / 2 + (dimensions.gap)" y="0" text-anchor="middle">Di</text>
         <text :x="dimensions.bar.width * 2 + dimensions.bar.width / 2 + (dimensions.gap * 2)" y="0" text-anchor="middle">Mi</text>
@@ -78,62 +78,17 @@ function getBarHeight(value, height) {
         <text :x="dimensions.bar.width * 5 + dimensions.bar.width / 2 + (dimensions.gap * 5)" y="0" text-anchor="middle" fill="#6b7280">Sa</text>
         <text :x="dimensions.bar.width * 6 + dimensions.bar.width / 2 + (dimensions.gap * 6)" y="0" text-anchor="middle" fill="#6b7280">So</text>
       </g>
-      <rect
-        x="0"
-        :y="dimensions.bar.height - ((graphData.Montag / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
-        :width="dimensions.bar.width"
-        :height="getBarHeight(graphData.Montag, dimensions.bar.height)"
-        :fill="colorScale(graphData.Montag)"
-        v-tooltip="`${format(graphData.Montag)} Verwarnungen`"
-      />
-      <rect
-        :x="dimensions.bar.width + dimensions.gap"
-        :y="dimensions.bar.height - ((graphData.Dienstag / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
-        :width="dimensions.bar.width"
-        :height="getBarHeight(graphData.Dienstag, dimensions.bar.height)"
-        :fill="colorScale(graphData.Dienstag)"
-        v-tooltip="`${format(graphData.Dienstag)} Verwarnungen`"
-      />
-      <rect
-        :x="(dimensions.bar.width + dimensions.gap) * 2"
-        :y="dimensions.bar.height - ((graphData.Mittwoch / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
-        :width="dimensions.bar.width"
-        :height="getBarHeight(graphData.Mittwoch, dimensions.bar.height)"
-        :fill="colorScale(graphData.Mittwoch)"
-        v-tooltip="`${format(graphData.Mittwoch)} Verwarnungen`"
-      />
-      <rect
-        :x="(dimensions.bar.width + dimensions.gap) * 3"
-        :y="dimensions.bar.height - ((graphData.Donnerstag / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
-        :width="dimensions.bar.width"
-        :height="getBarHeight(graphData.Donnerstag, dimensions.bar.height)"
-        :fill="colorScale(graphData.Donnerstag)"
-        v-tooltip="`${format(graphData.Donnerstag)} Verwarnungen`"
-      />
-      <rect
-        :x="(dimensions.bar.width + dimensions.gap) * 4"
-        :y="dimensions.bar.height - ((graphData.Freitag / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
-        :width="dimensions.bar.width"
-        :height="getBarHeight(graphData.Freitag, dimensions.bar.height)"
-        :fill="colorScale(graphData.Freitag)"
-        v-tooltip="`${format(graphData.Freitag)} Verwarnungen`"
-      />
-      <rect
-        :x="(dimensions.bar.width + dimensions.gap) * 5"
-        :y="dimensions.bar.height - ((graphData.Samstag / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
-        :width="dimensions.bar.width"
-        :height="getBarHeight(graphData.Samstag, dimensions.bar.height)"
-        :fill="colorScale(graphData.Samstag)"
-        v-tooltip="`${format(graphData.Samstag)} Verwarnungen`"
-      />
-      <rect
-        :x="(dimensions.bar.width + dimensions.gap) * 6"
-        :y="dimensions.bar.height - ((graphData.Sonntag / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
-        :width="dimensions.bar.width"
-        :height="getBarHeight(graphData.Sonntag, dimensions.bar.height)"
-        :fill="colorScale(graphData.Sonntag)"
-        v-tooltip="`${format(graphData.Sonntag)} Verwarnungen`"
-      />
+
+      <template v-for="n in 7">
+        <rect
+          :x="(dimensions.bar.width + dimensions.gap) * (n - 1)"
+          :y="dimensions.bar.height - ((graphData[n] / max) * dimensions.bar.height - dimensions.margins.top) + dimensions.margins.top"
+          :width="dimensions.bar.width"
+          :height="((graphData[n] / max) * dimensions.bar.height)"
+          :fill="colorScale(graphData[n])"
+          v-tooltip="`${format(graphData[n])} Verwarnungen`"
+        />
+      </template>
     </g>
   </svg>
 </template>
@@ -141,7 +96,8 @@ function getBarHeight(value, height) {
 <style scoped>
 .label {
   font-family: 'Inter var';
-  font-size: 1rem;
+  font-size: 0.75rem;
   font-weight: bold;
+  fill: rgb(31, 41, 55)
 }
 </style>
